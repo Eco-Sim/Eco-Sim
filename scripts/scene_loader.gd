5cede547b6551b69308a0b245d4ee975ba8f3a08
Nazuh <maverickl122806@gmail.com>
2025-04-18 18:34:49 -0400

    Formatting

--- scripts/scene_loader.gd
+++ scripts/scene_loader.gd
@@ -7,13 +7,10 @@ func load_level(level_name: String) -> void:
 	unload_level()
 
 	var level_path := "res://scenes/%s/%s.tscn" % [level_name, level_name]
-	print('level path:', level_path)
 	var level_resource := load(level_path)
 	if (!level_resource):
 		return
 	
-	# level_instance = level_resource.instantiate()
-	# add_child(level_instance)
 	get_tree().change_scene_to_file(level_path)
 
 func unload_level() -> void:
@@ -21,6 +18,3 @@ func unload_level() -> void:
 		level_instance.queue_free()
 	
 	level_instance = null
-
-# func _ready() -> void:
-# 	self.load_level('startmenu')
